bde5255d168c6b9baba556fae88e814637df1078
Adnan Raza <abc@gmail.com>
2026-02-26 14:59:03 +0530

    added request validation

---
+++ cmd/student-api/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/Mars-60/mini-project1.git/internal/config"
+	"github.com/Mars-60/mini-project1.git/internal/http/handlers/student"
+)
+
+func main() {
+	//load config
+	cfg := config.MustLoad()
+
+	//database setup
+	//setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("POST /api/students", student.New())
+
+	//setup server
+	server := http.Server{
+		Addr:    cfg.Address,
+		Handler: router,
+	}
+
+	slog.Info("server started ", slog.String("address", cfg.Address))
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("Failed to start server")
+		}
+	}()
+
+	<-done
+
+	slog.Info("Shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
+	}
+
+	slog.Info("server shutdown successfully")
+}
